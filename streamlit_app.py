41b1a303907718d370ab9b1c6e616986722779b9
Najmi Ariffin <booluckgmie@gmail.com>
2023-08-16 14:57:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,18 @@ import pandas as pd
 import streamlit as st
 import re
 import numpy as np
-import openpyxl
 
-# df = pd.read_excel("PADU_PGK Makanan, Beta & Bukan Makanan 2022.xlsx", sheet_name='Data2019')
+# Load dataset from CSV file
+kump_pdptn = pd.read_csv("kump_pdptn2022.csv")
+
+# Load other data
 food_PLI = pd.read_excel("PADU_PGK Makanan, Beta & Bukan Makanan 2022.xlsx", sheet_name='Data PGK Makanan')
 food_PLI.columns = ['Kumpulan Umur', 'Umur', 'Kalori', 'Negeri', 'Strata', 'totalFood_PLI']
+
+nonfood_PLI = pd.read_excel("pgk/PADU_PGK Makanan, Beta & Bukan Makanan 2022.xlsx", sheet_name='Data PGK B.Makanan (p)')
+nonfood_PLI = nonfood_PLI[['Negeri', 'Strata', 'p_cloth', 'p_rent', 'p_durable', 'p_transport', 'p_other', 'totalNP_PLI']]
+nonfood_PLI.columns = ['Negeri', 'Strata', 'p_cloth', 'p_rent', 'p_durable', 'p_transport', 'p_other', 'totalNFood_PLI']
+
 # Assuming food_PLI is your DataFrame
 def map_gender(row):
     if 'Lelaki' in row['Kumpulan Umur']:
@@ -20,13 +27,6 @@ food_PLI['Jantina'] = food_PLI.apply(map_gender, axis=1)
 food_PLI['Kumpulan Umur'] = food_PLI['Kumpulan Umur'].apply(lambda x: re.sub(r'(Lelaki|Perempuan)', '', x, flags=re.IGNORECASE))
 food_PLI['totalFood_PLI'] = pd.to_numeric(food_PLI['totalFood_PLI'], errors='coerce')
 food_PLI['totalFood_PLI'] = food_PLI['totalFood_PLI'].replace([np.nan, np.inf, -np.inf], 0).astype(int)
-
-
-# df = pd.read_excel("PADU_PGK Makanan, Beta & Bukan Makanan 2022.xlsx", sheet_name='Data2019')
-nonfood_PLI = pd.read_excel("PADU_PGK Makanan, Beta & Bukan Makanan 2022.xlsx", sheet_name='Data PGK B.Makanan (p)')
-nonfood_PLI = nonfood_PLI[['Negeri', 'Strata', 'p_cloth', 'p_rent', 'p_durable', 'p_transport', 'p_other', 'totalNP_PLI']]
-nonfood_PLI['totalNP_PLI'] = nonfood_PLI['totalNP_PLI'].astype(int)
-nonfood_PLI.columns = ['Negeri', 'Strata', 'p_cloth', 'p_rent', 'p_durable', 'p_transport', 'p_other', 'totalNFood_PLI']
 
 # Run the Streamlit app
 if __name__ == '__main__':
@@ -43,6 +43,7 @@ if __name__ == '__main__':
     # ... Add more attribution or information if needed ...
 
     st.sidebar.write('Powered by Streamlit')
+
 
 # Create interactive widgets
 state_dropdown = st.selectbox('State:', ['select', 'Johor', 'Kedah', 'Kelantan', 'Malaysia', 'Melaka', 'Negeri Sembilan', 'Pahang', 'Perak', 'Perlis', 'Pulau Pinang', 'Sabah', 'Sarawak', 'Selangor', 'Terengganu', 'W.P. Kuala Lumpur', 'W.P. Labuan', 'W.P. Putrajaya'])
@@ -62,28 +63,18 @@ if st.button('Calculate'):
         selected_strata = strata_dropdown
         pendapatan = pendapatan_input
 
-        # Given dataset kumpulan pendapatan
-        kump_pdptn = [
-                        (0, 2499, 'B1', 'B40'),
-                        (2500, 3170, 'B2', 'B40'),
-                        (3171, 3970, 'B3', 'B40'),
-                        (3971, 4849, 'B4', 'B40'),
-                        (4850, 5880, 'M1', 'M40'),
-                        (5881, 7100, 'M2', 'M40'),
-                        (7111, 8700, 'M3', 'M40'),
-                        (8701, 10959, 'M4', 'M40'),
-                        (10960, 15040, 'T1', 'T20'),
-                        (15040, 1000000, 'T2', 'T20')
-                    ]
+        kump_pdptn = pd.read_csv("kump_pdptn2022.csv")
 
-        # Finding the corresponding values for kump_decile and kump_main
         kump_decile = None
         kump_main = None
-        for row in kump_pdptn:
-            min_val, max_val, kump_decile_val, kump_main_val = row
-            if min_val <= pendapatan <= max_val:
-                kump_decile = kump_decile_val
-                kump_main = kump_main_val
+
+        for index, row in kump_pdptn.iterrows():
+            min_val = row['income_min']
+            max_val = row['income_max']
+
+            if min_val <= pendapatan <= max_val and row['State'] == selected_state:
+                kump_decile = row['kump_decile']
+                kump_main = row['kump_main']
                 break
 
         if selected_age == 'select' or selected_gender == 'select' or selected_strata == 'select':
